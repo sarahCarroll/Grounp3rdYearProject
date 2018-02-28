f94f907c79e33bc05760b065496a5607239d3c49
Abigail Cul <g00334291@gmit.ie>
2018-02-28 18:19:48 +0000

    database with ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
@@ -21,6 +22,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    //add httpmodule to the imports 
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,7 +37,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public myDataArray:any[];
+  //empty member variable called posts - to pull in remote server
+  posts: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public http: Http) {
   }
 
+
+  ionViewWillEnter(){
+   
+
+var parameters={
+  _fn:'getHerdNumbers'
+}
+
+this.http.post('http://35.205.81.28/api/',parameters).subscribe((data)=>{
+  
+  console.log(JSON.parse(data['_body']));
+  this.myDataArray = (JSON.parse(data['_body']));
+},
+err=>{console.log(err)});
+
+
+    
+  console.log("hello just called get");
+  }
 }
