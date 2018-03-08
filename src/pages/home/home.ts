5384716a4d0d8d5ed8f8d02a344ae86813e8fa70
sarahc123 <g00330821@gmit.ie>
2018-03-08 14:40:52 +0000

    trying to get length of array

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,6 +10,7 @@ import 'rxjs/add/operator/map';
 export class HomePage {
   //public array to store the information from database
   public myDataArray: any[];
+  public count: any[];
 
 
 
@@ -24,15 +25,16 @@ export class HomePage {
       _fn: 'getHerdNumbers'
     }
 
+
     this.http.post('http://35.205.81.28/api/', parameters).subscribe((data) => {
 
       console.log(JSON.parse(data['_body']));
       this.myDataArray = (JSON.parse(data['_body']));
     },
+
+
+
       err => { console.log(err) });
-
-
-
     console.log("hello just called get");
   }
 }
