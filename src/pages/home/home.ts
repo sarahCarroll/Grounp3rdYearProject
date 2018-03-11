941b6dc4d7c5bf32d326efcc091d7950c8262954
sarahc123 <g00330821@gmit.ie>
2018-03-11 09:25:00 +0000

    added new function to php, no of animals

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,7 +3,7 @@ import { NavController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-import {AddPage} from '../add/add';
+import { AddPage } from '../add/add';
 
 
 @Component({
@@ -13,7 +13,7 @@ import {AddPage} from '../add/add';
 export class HomePage {
   //public array to store the information from database
   public myDataArray: any[];
-  public count: any[];
+  //public myCountArray: any[];
 
 
   constructor(public navCtrl: NavController, public http: Http) {
@@ -26,11 +26,22 @@ export class HomePage {
       _fn: 'getHerdNumbers'
     }
 
+    var changes = {
+      _fn: 'getNumberOfAnimals'
+    }
+
 
     this.http.post('http://35.205.81.28/api/', parameters).subscribe((data) => {
 
       console.log(JSON.parse(data['_body']));
       this.myDataArray = (JSON.parse(data['_body']));
+    },
+      err => { console.log(err) });
+
+    this.http.post('http://35.205.81.28/api/', changes).subscribe((data) => {
+
+      console.log(JSON.parse(data['_body']));
+      //this.myCountArray = (JSON.parse(data['_body']));
     },
 
 
