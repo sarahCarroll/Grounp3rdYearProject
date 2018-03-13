d7a8fee4c78e3d2b357c74de6bc6632aea2249cb
sarahc123 <g00330821@gmit.ie>
2018-03-13 11:37:15 +0000

    console

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,7 +13,7 @@ import { AddPage } from '../add/add';
 export class HomePage {
   //public array to store the information from database
   public myDataArray: any[];
-  //public myCountArray: any[];
+  public myCountArray: any[];
 
 
   constructor(public navCtrl: NavController, public http: Http) {
@@ -41,7 +41,7 @@ export class HomePage {
     this.http.post('http://35.205.81.28/api/', changes).subscribe((data) => {
 
       console.log(JSON.parse(data['_body']));
-      //this.myCountArray = (JSON.parse(data['_body']));
+      //this.myCountArray = ((data['_body']));
     },
 
 
