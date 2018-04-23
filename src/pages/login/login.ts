f50edb8354fed012a5799327109d08a89a2beb47
Abigail Cul <g00334291@gmit.ie>
2018-04-23 19:08:44 +0100

    login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -14,9 +14,44 @@ export class LoginPage {
   tabsPage = TabsPage;
 
 
+  Name: string;
+  password: string;
+  herdNo: string;
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, private alertCtrl: AlertController) {
   }
 
+  async login() {
 
-  
+    //this.ref.orderByChild("email").equalTo
+
+  var i = 0;
+
+  var add = {
+    _fn: 'login',
+    name: this.Name,
+    password: this.password,
+    farmNo: this.herdNo,
+  }
+
+  /* try {
+     this.http.post('http://104.199.57.94/api/', add).subscribe((data) => {
+       console.log(data['_body'])
+       this.navCtrl.setRoot(TabsPage, { sid: this.herdNo, name: this.Name, password: this.password })
+       console.log(data.toString)
+     }, error => {
+       console.log(error);// Error 
+     });
+   }
+ */
+
+  this.http.post('http://104.199.57.94/api/', add).subscribe((data) => {
+    console.log(data['_body'])
+    this.navCtrl.setRoot(TabsPage, { sid: this.herdNo, name: this.Name, password: this.password })
+    console.log(data.toString)
+  }, error => {
+    console.log("Cannot login");// Error 
+  });
+
 }
+}
+  
